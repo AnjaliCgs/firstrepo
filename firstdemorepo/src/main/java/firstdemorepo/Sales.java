ca9541e37c37543bb28e406e87dedf261a5baf22
AnjaliCgs <anjali-subhash.agarwal@capgemini.com>
2021-08-16 16:56:50 +0530

    Sales added

---
+++ firstdemorepo/src/main/java/firstdemorepo/Sales.java
@@ -0,0 +1,12 @@
+package firstdemorepo;
+
+public class Sales {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String a = "ABCD";
+		System.out.println(a);
+
+	}
+
+}
