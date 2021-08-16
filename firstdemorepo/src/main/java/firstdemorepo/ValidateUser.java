a543ac5298a50ce68f5987e9595fa04b91dbb505
AnjaliCgs <anjali-subhash.agarwal@capgemini.com>
2021-08-16 17:47:22 +0530

    Validating User

---
+++ firstdemorepo/src/main/java/firstdemorepo/ValidateUser.java
@@ -0,0 +1,10 @@
+package firstdemorepo;
+
+public class ValidateUser {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Validating USer");
+	}
+
+}
